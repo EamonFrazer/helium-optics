207b4440747620ec0d2bba5d2cea4bb6159ae79c
Azrrael <vcamargo.e@gmail.com>
2018-10-30 19:12:03 -0500

    test: Test result

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-from geometry.geometry import Layer
+from geometry.geometry import Layer, snell
+import matplotlib.pyplot as plt
+import numpy as np
 
-layer = Layer({
+air = Layer({
+    'input' : 0,
+    'refraction' : 1
+})
+
+poli = Layer({
     'input' : -5,
-    'output' : 0,
     'refraction' : 1.5
 })
 
-for i in range(45, 90):
-    print("input: {:.2f}, output: {:.2f}".format(i, layer.proc(i)['b4']))
+glass = Layer({
+    'input' : 0,
+    'refraction' : 1.2
+})
+
+x = []
+y = []
+
+for i in range(45, 85):
+    first_ref = snell(i, air, poli)[0]
+    second_ref = snell(first_ref, poli, air)[0]
+    third_ref = snell(second_ref, air, glass)[0]
+    x.append(i)
+    y.append(third_ref)
+    print("r0: {:.2f}\tr1: {:.2f}\tr2: {:.2f}\tr3: {:.2f}".format(i ,first_ref, second_ref, third_ref))
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+plt.title("Refraction Result")
+plt.xlabel(r'input($\beta_1$)')
+plt.ylabel(r'output($\beta_2$)')
+plt.show()
