2d30014675faba4f0a27507a2447b43a3945a6f3
Eamon Frazer <fraea318@student.otago.ac.nz>
2018-11-08 17:36:29 -0500

    test: Separate air layers
    Allowing the interior of the plastic panel to have an angle (specified as the input angle of the second air layer, air2)

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from geometry.geometry import Layer, snell
 import matplotlib.pyplot as plt
 import numpy as np
 
-air = Layer({
+air1 = Layer({
     'input' : 0,
+    'refraction' : 1
+})
+
+air2 = Layer({
+    'input' : 5,
     'refraction' : 1
 })
 
@@ -14,16 +19,16 @@ poli = Layer({
 
 glass = Layer({
     'input' : 0,
-    'refraction' : 1.2
+    'refraction' : 1.5
 })
 
 x = []
 y = []
 
 for i in range(45, 85):
-    first_ref = snell(i, air, poli)[0]
-    second_ref = snell(first_ref, poli, air)[0]
-    third_ref = snell(second_ref, air, glass)[0]
+    first_ref = snell(i, air1, poli)[0]
+    second_ref = snell(first_ref, poli, air2)[0]
+    third_ref = snell(second_ref, air2, glass)[0]
     x.append(i)
     y.append(third_ref)
     print("r0: {:.2f}\tr1: {:.2f}\tr2: {:.2f}\tr3: {:.2f}".format(i ,first_ref, second_ref, third_ref))
